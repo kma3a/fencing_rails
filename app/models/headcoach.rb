5602653ea4517c1872b835e3bba3535ba4109380
Kelsey <kma3a@virginia.edu>
2014-08-13 14:07:55 -0500

    added some things but still haven't figured out how to get name on sign in page

--- app/models/headcoach.rb
+++ app/models/headcoach.rb
@@ -1,4 +1,4 @@
 class Headcoach < ActiveRecord::Base
   has_many :teams
-  devise :database_authenticatable, :timeoutable
+  devise :database_authenticatable, :registerable, :recoverable, :timeoutable
 end

d0271fc2f794d5e434be37e90d6bb267e7f2d0ff
Kelsey <kma3a@virginia.edu>
2014-08-18 17:17:33 -0500

    added show for the coaches page to go to after sign in

---
+++ app/controllers/coaches_controller.rb
@@ -0,0 +1,7 @@
+class CoachesController < ApplicationController
+
+  def show
+    @coach = Coach.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :headcoaches, :only => [:show]
   resources :coaches, :only => [:show]
+  resources :teams
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

24d35e86a657285469068dda51e92355db378311
Kelsey <kma3a@virginia.edu>
2014-08-18 16:45:28 -0500

    changed db so there is not default for name

--- db/migrate/20140808212727_create_headcoaches.rb
+++ db/migrate/20140808212727_create_headcoaches.rb
@@ -1,7 +1,7 @@
 class CreateHeadcoaches < ActiveRecord::Migration
   def change
     create_table :headcoaches do |t|
-      t.string :name, null:false, default: "Coach"
+      t.string :name, null:false
       t.string :email, null:false, default: ""
       t.string :encrypted_password, null:false, default: ""
       t.timestamps

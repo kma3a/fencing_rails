2513181f7a26d13526ae32284b16dac7f05d6fa7
Kelsey <kma3a@virginia.edu>
2014-08-19 11:45:59 -0500

    Can now delete teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,13 +5,19 @@ class TeamsController < ApplicationController
   end
   
   def create
-    p params
     @team = Team.create(name: params[:team][:name], headcoach_id: current_headcoach.id)
     if @team
       redirect_to headcoach_path(current_headcoach.id)
     else
       render 'new'
     end
+  end
+
+  def destroy
+    p params
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to headcoach_path(current_headcoach.id)
   end
 
 end

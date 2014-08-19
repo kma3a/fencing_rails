20e72cec0abc286d4a1716400079b963e6b1109d
Kelsey <kma3a@virginia.edu>
2014-08-19 11:32:28 -0500

    made it so it will create the team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,5 +4,13 @@ class TeamsController < ApplicationController
     @team = Team.new()
   end
   
+  def create
+    @team = Team.create(name: params[:name], headcoach_id: current_headcoach.id)
+    if @team
+      redirect_to headcoach_path(current_headcoach.id)
+    else
+      render 'new'
+    end
+  end
 
 end

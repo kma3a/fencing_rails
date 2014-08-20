f259fe74d4dd6123adfbc9bfc419549875c3cd67
Kelsey <kma3a@virginia.edu>
2014-08-20 18:12:27 -0500

    team controller is registering and works for creating a new contact

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,35 +1,17 @@
 require 'rails_helper'
 
-describe TeamsController do
-  describe '#create' do
-  let(:params) {{name: 'Fighting Otters', headcoach_id: 1}}
-  let(:team) {Team.new(params)}
-  let(:action) {post :create, params}
+RSpec.describe TeamsController, :type => :controller do
+  let(:headcoach) {Headcoach.create({name: 'matt', email: 'vanillabear@google.com', password: 'otterpoop', password_confirmation: 'otterpoop'})}
 
-  before do
-    Post.should_recieve(:new).and_return(post)
+  before {sign_in headcoach}
+  describe 'POST #create' do
+
+    it "should create a team" do
+      expect{ post :create, team: {name: 'Fighting Otters', headcoah_id: 1} }.to change(Team,:count).by(1)
+    end
   end
 
-  context 'on success' do
-    
-    before do
-      post.should_receive(:save).and_return(true)
-    end
-
-    it 'renders success' do
-      action
-      expect(responce).to be_success
-    end
-
-    it 'redirects' do
-      action
-      expect(responce).to be_redirected
-    end
-
- end
-
  end
 
 
 
-end
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,11 +7,6 @@ RSpec.describe WelcomeController, :type => :controller do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
-
-    it 'renders index template' do
-      get :index
-      expect(response).to render_template("index")
-    end
   end
 
 end

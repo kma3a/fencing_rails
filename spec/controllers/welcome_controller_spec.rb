1dc4ff6c13f7ad4275454666a227ce6c04956ce0
Kelsey <kma3a@virginia.edu>
2014-08-20 15:10:49 -0500

    finished welcome controller tests and am learning how to spell response

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe WelcomeController, :type => :controller do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+
+    it 'renders index template' do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 
 end

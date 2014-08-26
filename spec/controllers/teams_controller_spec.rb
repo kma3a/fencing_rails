c21260640a1bbc01039aad883292d52a19bddd43
Kelsey <kma3a@virginia.edu>
2014-08-26 14:35:42 -0500

    added tests for redirects and made subject to have it be less repeat

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe TeamsController, :type => :controller do
 
   before {sign_in headcoach}
   describe 'POST #create' do
+    subject {post :create, team: {name: 'Fighting Otters', headcoah_id: 1} }
+    context 'valid attributes' do
+      it "should create a team" do
+        expect{subject}.to change(Team,:count).by(1)
+      end
 
-    it "should create a team" do
-      expect{ post :create, team: {name: 'Fighting Otters', headcoah_id: 1} }.to change(Team,:count).by(1)
+      it 'redirects to the new contact' do
+        expect(subject).to redirect_to("/headcoaches/#{assigns(:team).headcoach.id}")
+      end
     end
   end
+
+  describe 'GET #new' do
+  end
+
 
  end
 

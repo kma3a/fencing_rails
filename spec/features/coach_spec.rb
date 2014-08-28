5c5605f2cf7fbb923d48e13236ebb08dc68a0a0c
Kelsey <kma3a@virginia.edu>
2014-08-28 16:27:02 -0500

    added validation for presence of name for coach

--- spec/features/coach_spec.rb
+++ spec/features/coach_spec.rb
@@ -11,5 +11,17 @@ feature 'coach sign up' do
 
     expect{ click_button "Sign up"}.to change(Coach, :count).by(1)
   end
+  
+
+  scenario 'does not sign up coach with invalid input' do
+    visit('/coaches/sign_up')
+
+    fill_in("Name", with: "")
+    fill_in("Email", with: "mark@gmail.com")
+    fill_in("Password", with: "ameliasteal")
+    fill_in("Password confirmation", with: "ameliasteal")
+
+    expect{ click_button "Sign up"}.to change(Coach, :count).by(0)
+  end
 
 end
--- spec/models/coach_spec.rb
+++ spec/models/coach_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Coach do
   it { should have_many(:team_coaches)}
   it { should have_many(:teams).through(:team_coaches)}  
+  it { should validate_presence_of(:name)}
 end

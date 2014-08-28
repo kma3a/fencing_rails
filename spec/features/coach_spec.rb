a0357aacaa6dea7d1d5d54db433212492a07718e
Kelsey <kma3a@virginia.edu>
2014-08-28 16:48:59 -0500

    not sure if needed but added a test to make sure it will have current path be different if not valid

--- spec/features/coach_spec.rb
+++ spec/features/coach_spec.rb
@@ -57,7 +57,7 @@ feature 'coach sign up' do
     expect{ click_button "Sign up"}.to change(Coach, :count).by(0)
   end
 
-  scenario 'signs up coach with valid input' do
+  scenario 'makes sure will redirect if valid' do
     visit('/coaches/sign_up')
 
     fill_in("Name", with: "Coach Mark")
@@ -69,5 +69,16 @@ feature 'coach sign up' do
     expect(current_path).to eq(coach_path(Coach.last))
   end
 
+  scenario 'will render same view if not valid' do
+    visit('/coaches/sign_up')
+
+    fill_in("Name", with: "Coach Mark")
+    fill_in("Email", with: "")
+    fill_in("Password", with: "ameliasteal")
+    fill_in("Password confirmation", with: "ameliasteal")
+    click_button "Sign up"
+
+    expect(current_path).to eq('/coaches')
+  end
 
 end

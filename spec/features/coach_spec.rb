9e8b4712eb777f945d2d63d4c00ccceb10a57f55
Kelsey <kma3a@virginia.edu>
2014-08-28 16:32:32 -0500

    made tests for the invalid input for the sign up

--- spec/features/coach_spec.rb
+++ spec/features/coach_spec.rb
@@ -24,4 +24,39 @@ feature 'coach sign up' do
     expect{ click_button "Sign up"}.to change(Coach, :count).by(0)
   end
 
+  scenario 'does not sign up coach with no email' do
+    visit('/coaches/sign_up')
+
+    fill_in("Name", with: "Coach Mark")
+    fill_in("Email", with: "")
+    fill_in("Password", with: "ameliasteal")
+    fill_in("Password confirmation", with: "ameliasteal")
+
+    expect{ click_button "Sign up"}.to change(Coach, :count).by(0)
+  end
+
+  scenario 'does not sign up coach with no password' do
+    visit('/coaches/sign_up')
+
+    fill_in("Name", with: "Coach Mark")
+    fill_in("Email", with: "mark@gmail.com")
+    fill_in("Password", with: "")
+    fill_in("Password confirmation", with: "ameliasteal")
+
+    expect{ click_button "Sign up"}.to change(Coach, :count).by(0)
+  end
+
+  scenario 'does not sign up coach with no password confirmation' do
+    visit('/coaches/sign_up')
+
+    fill_in("Name", with: "Coach Mark")
+    fill_in("Email", with: "mark@gmail.com")
+    fill_in("Password", with: "ameliasteal")
+    fill_in("Password confirmation", with: "")
+
+    expect{ click_button "Sign up"}.to change(Coach, :count).by(0)
+  end
+
+
+
 end

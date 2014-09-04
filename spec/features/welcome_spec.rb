4fab19852c8f34ba5e4e92dd4e5774cfb6dea0a1
Kelsey <kma3a@virginia.edu>
2014-09-03 23:30:30 -0500

    made test to redirect when the coach link is clicked

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -12,4 +12,11 @@ feature "welcome page tests" do
     click_link("Head Coach Sign In")
     expect(current_path).to eq(new_headcoach_session_path)
   end
+
+  scenario "When click on coach link will go to login page" do
+    visit("/")
+    click_link("Coach Sign In")
+    expect(current_path).to eq(new_coach_session_path)
+  end
+
 end

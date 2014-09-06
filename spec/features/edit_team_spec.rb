f1e982f1c25d8d3050ddcb426a562b23ec9aa743
Kelsey <kma3a@virginia.edu>
2014-09-05 19:23:09 -0500

    added test so that it will render headcoach if valid

--- spec/features/edit_team_spec.rb
+++ spec/features/edit_team_spec.rb
@@ -1,3 +1,22 @@
 require "rails_helper"
 
+include Warden::Test::Helpers
+Warden.test_mode!
+
+feature "edit team" do
+    
+    let(:headcoach) {Headcoach.create({name: "matt", email: 'vanillabear@otters.com', password: 'otterpoop', password_confirmation: 'otterpoop'})}
+    let(:team) {Team.create({name: "Otters", headcoach_id: headcoach.id})}
+
+    before{login_as(headcoach, scope: :headcoach)}
+    before{ headcoach.teams << team}
+
+  scenario "allows team to be edited with valid input" do
+    visit(edit_team_path(team.id))
+    fill_in('team[name]', with: "Fighting Otters")
+    click_button "Submit"
+    expect(current_path).to eq(headcoach_path(headcoach.id))
+  end
+end
+
 

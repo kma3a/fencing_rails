8ecab72a9802e42f7a759e19d23870d378bc5aaf
Kelsey <kma3a@virginia.edu>
2014-09-05 19:02:15 -0500

    made a test for the teams edit link

--- spec/features/headcoach_show_spec.rb
+++ spec/features/headcoach_show_spec.rb
@@ -6,8 +6,10 @@ Warden.test_mode!
 feature "Headcoach show page" do
     
     let(:headcoach) {Headcoach.create({name: "matt", email: 'vanillabear@otters.com', password: 'otterpoop', password_confirmation: 'otterpoop'})}
+    let(:team) {Team.create({name: "Otters", headcoach_id: headcoach.id})}
 
     before{login_as(headcoach, scope: :headcoach)}
+    before{headcoach.teams << team}
 
   scenario "can click in link new team" do
 
@@ -15,6 +17,13 @@ feature "Headcoach show page" do
     click_link "Create Team"
     expect(current_path).to eq(new_team_path)
 
+  end
+
+  scenario "click on the edit button and it will render edit" do
+    visit(headcoach_path(headcoach.id))
+    expect(page).to have_content("Your Teams")
+    click_link "Edit"
+    expect(current_path).to eq(edit_team_path(team.id))
   end
 
 end

9c5ca8143754bc413d5ba2d91c7c6c290ff95034
Kelsey <kma3a@virginia.edu>
2014-09-24 13:19:09 -0500

    added secret key to students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,13 @@
+require 'securerandom'
 class Student < ActiveRecord::Base
   has_many :team_students
   has_many :teams, through: :team_students
   has_many :participants
   has_many :events, through: :participants
+
+  before_create :create_key
+
+  def create_key
+    self.secret_key = SecureRandom.base64(9).sub(/[\/\\]/, "1")
+  end
 end

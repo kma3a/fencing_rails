2456ea279d8cbfb7dde82c2600047adc73ae1196
Kelsey <kma3a@virginia.edu>
2014-10-02 16:30:19 -0500

    added authentication for the events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,18 @@
 class EventsController < ApplicationController
+  before_filter :authenticate!, except: [:show]
 
   def new
     @event = Event.new()
+  end
+
+  private
+
+  def authenticate!
+    if headcoach_signed_in?
+    
+    else
+      authenticate_coach!
+    end
   end
 
 

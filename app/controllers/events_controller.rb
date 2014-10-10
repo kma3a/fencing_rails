521384e4faccaf54e11697fa9e2110b8baf91888
Kelsey <kma3a@virginia.edu>
2014-10-10 15:52:21 -0500

    made it so events is accessed through teams so that the team will be put on event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,14 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new()
+    @team = Team.find(params[:team_id])
   end
   
   def create
+    @team = Team.find(params[:team_id])
     @event = Event.new({event_title: params[:event][:event_title], participant_count: params[:event][:participant_count], team_id: params[:team_id]})
     if @event.save
-      redirect_to event_path(@event.id)
+      redirect_to team_event_path(@team, @event.id)
     end
   end
 

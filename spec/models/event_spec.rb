069d1ffdac65dd2019386b31caaa8fe5ec69ce99
Kelsey <kma3a@virginia.edu>
2014-10-10 13:45:38 -0500

    added secret_key to the events

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,10 @@ describe Event do
   it { should belong_to(:team)}
   it { should have_many(:participants)}
   it { should have_many(:students).through(:participants)}
+  let(:headcoach) {Headcoach.create({name: "Mark", email: "marrt@lsejrle.com", password: "beavers", password_confirmation: "beavers"})}
+  let(:team) {Team.create({name:"Otters", headcoach_id: headcoach.id})}
+  let(:event) {Event.create({event_title: "10/14/14", team_id: team.id})}
+  it 'should give a event a secret key' do
+    expect(event.secret_key).to_not eq(nil)
+  end
 end

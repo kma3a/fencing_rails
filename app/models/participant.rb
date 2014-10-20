e1b446e74986e03f3a830bd004fdf4e56f039159
Kelsey <kma3a@virginia.edu>
2014-10-20 16:31:42 -0500

    added the sanitize so bout_results can be a hash and created the hash in the beginning

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,16 @@
 class Participant < ActiveRecord::Base
   belongs_to :event
   belongs_to :student
+  serialize :bout_results, Hash
+
+  before_save :add_results
+
+  def add_results
+    (1..self.event.participant_count).each do |num|
+      if num != self.bout_number
+        self.bout_results[num] = nil
+      end
+    end
+  end
+
 end

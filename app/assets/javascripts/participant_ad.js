7216c2843d0dfc9fb5c27fdf77e2a0caa40ca1fc
Kelsey <kma3a@virginia.edu>
2014-10-23 14:02:53 -0500

    added the javascript to make it so that you can add more participants

--- app/assets/javascripts/participant_ad.js
+++ app/assets/javascripts/participant_ad.js
@@ -1,8 +1,18 @@
+
+  var participantNode = "<li><input type='text' name='event[participants][]'><button class='remove-button'>X</button></li>"
+
 $(document).ready(function(){
-  var participantNode = "<li><input type='text' name='event[participants][]'></li>"
+  
 
+$('#new_event').on('click','.add_participant', function(event){
+  event.preventDefault();
+  $($(this).siblings(' ol')).append(participantNode);
+});
 
-  //$('#new_event > ol').append(participantNode);
+$('#new_event').on('click','.remove-button', function(event){
+  event.preventDefault();
+  $(this).parent().remove();
+});
 
 
 
